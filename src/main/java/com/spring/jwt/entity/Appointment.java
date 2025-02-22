f4fb8634daacc4732d97b4cc1099344806d30c76
tkraj888 <tusharck88@gmail.com>
2025-02-22 13:49:16 +0530

    1st ok

---
+++ src/main/java/com/spring/jwt/entity/Appointment.java
@@ -0,0 +1,168 @@
+package com.spring.jwt.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+
+@Entity
+public class Appointment {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Integer appointmentId;
+
+    @Column(length = 45)
+    private String appointmentDate;
+
+    @Column(length = 45)
+    private String customerName;
+
+    @Column(length = 45)
+    private String mobileNo;
+
+    @Column(length = 45)
+    private String vehicleNo;
+
+    @Column(length = 45)
+    private String vehicleMaker;
+
+    @Column(length = 45)
+    private String vehicleModel;
+
+    @Column(length = 45)
+    private String manufacturedYear;
+
+    @Column(length = 45)
+    private String kilometerDriven;
+
+    @Column(length = 45)
+    private String fuelType;
+
+    @Column(length = 45)
+    private String workType;
+
+    @Column(length = 45)
+    private String vehicleProblem;
+
+    @Column(length = 45)
+    private String pickUpAndDropService;
+
+    public Integer getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(final Integer appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(final String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(final String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(final String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(final String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getVehicleMaker() {
+        return vehicleMaker;
+    }
+
+    public void setVehicleMaker(final String vehicleMaker) {
+        this.vehicleMaker = vehicleMaker;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(final String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public String getManufacturedYear() {
+        return manufacturedYear;
+    }
+
+    public void setManufacturedYear(final String manufacturedYear) {
+        this.manufacturedYear = manufacturedYear;
+    }
+
+    public String getKilometerDriven() {
+        return kilometerDriven;
+    }
+
+    public void setKilometerDriven(final String kilometerDriven) {
+        this.kilometerDriven = kilometerDriven;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(final String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(final String workType) {
+        this.workType = workType;
+    }
+
+    public String getVehicleProblem() {
+        return vehicleProblem;
+    }
+
+    public void setVehicleProblem(final String vehicleProblem) {
+        this.vehicleProblem = vehicleProblem;
+    }
+
+    public String getPickUpAndDropService() {
+        return pickUpAndDropService;
+    }
+
+    public void setPickUpAndDropService(final String pickUpAndDropService) {
+        this.pickUpAndDropService = pickUpAndDropService;
+    }
+
+}
---
+++ src/main/java/com/spring/jwt/entity/Sale.java
@@ -0,0 +1,157 @@
+package com.spring.jwt.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+
+@Entity
+public class Sale {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Integer stockId;
+
+    @Column(nullable = false, length = 45)
+    private String invoiceNo;
+
+    @Column(length = 45)
+    private String invDate;
+
+    @Column(length = 45)
+    private String name;
+
+    @Column(length = 45)
+    private String address;
+
+    @Column(length = 45)
+    private String mobileNo;
+
+    @Column(length = 45)
+    private String gstin;
+
+    @Column(length = 45)
+    private String spareName;
+
+    @Column(length = 45)
+    private String mrp;
+
+    @Column(length = 45)
+    private String rate;
+
+    @Column(length = 45)
+    private String qty;
+
+    @Column(length = 45)
+    private String gst;
+
+    public Integer getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(final Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getInvoiceNo() {
+        return invoiceNo;
+    }
+
+    public void setInvoiceNo(final String invoiceNo) {
+        this.invoiceNo = invoiceNo;
+    }
+
+    public String getInvDate() {
+        return invDate;
+    }
+
+    public void setInvDate(final String invDate) {
+        this.invDate = invDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(final String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getGstin() {
+        return gstin;
+    }
+
+    public void setGstin(final String gstin) {
+        this.gstin = gstin;
+    }
+
+    public String getSpareName() {
+        return spareName;
+    }
+
+    public void setSpareName(final String spareName) {
+        this.spareName = spareName;
+    }
+
+    public String getMrp() {
+        return mrp;
+    }
+
+    public void setMrp(final String mrp) {
+        this.mrp = mrp;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(final String rate) {
+        this.rate = rate;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(final String qty) {
+        this.qty = qty;
+    }
+
+    public String getGst() {
+        return gst;
+    }
+
+    public void setGst(final String gst) {
+        this.gst = gst;
+    }
+
+}
